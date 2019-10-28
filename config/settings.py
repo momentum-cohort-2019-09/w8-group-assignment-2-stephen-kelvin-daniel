6ac0568c8c5f09cc090c062a217ae4e1d5465331
danielwalker <dawalker2017@outlook.com>
2019-10-28 14:13:25 -0400

    |10.28.19|02:12pm| Created Django Project: Flash and config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Django Project:
+    'Flash'
+    
+    #Third-Parties:
+
 ]
 
 MIDDLEWARE = [
