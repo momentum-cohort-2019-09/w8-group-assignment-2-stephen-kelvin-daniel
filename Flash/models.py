39451e73aed8dc0ffe3ccd56ff6e1035afb8927a
danielwalker <dawalker2017@outlook.com>
2019-10-28 17:52:03 -0400

    Created User, Deck, and Card Models

--- Flash/models.py
+++ Flash/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Deck(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name="decks"
+                             )
+
+    title = models.CharField(
+        verbose_name="Deck Title",
+        max_length=50,
+        blank=False,
+    )
+
+    description = models.TextField(
+        verbose_name="Deck Description",
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Card(models.Model):
+    deck = models.ForeignKey(Deck,
+                             on_delete=models.CASCADE,
+                             related_name="cards"
+                             )
+
+    question = models.TextField(
+        verbose_name="Question:",
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+
+    answer = models.TextField(
+        verbose_name="Answer:",
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+
+    total_guesses = models.IntegerField(
+        default=0,
+    )
+
+    total_correct_guesses = models.IntegerField(
+        default=0,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django-Project:
+AUTH_USER_MODEL = 'Flash.User'
