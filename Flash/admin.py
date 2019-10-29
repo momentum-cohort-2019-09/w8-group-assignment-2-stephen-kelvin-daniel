3327eda2e1e94b908608cb4a8c1fc5278f304b6b
danielwalker <dawalker2017@outlook.com>
2019-10-29 01:31:50 -0400

    Created admin interface |01:31am|10.29.19|

--- Flash/admin.py
+++ Flash/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from Flash.models import Deck, Card
 
-# Register your models here.
+
+class CardInline(admin.TabularInline):
+    model = Card
+    extra = 0
+
+
+class DeckAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'number_of_cards',
+        'created_at',
+    )
+
+    inlines = [CardInline]
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = (
+        'question',
+        'answer',
+        'created_at',
+    )
+
+
+admin.site.register(Deck, DeckAdmin, )
+admin.site.register(Card, CardAdmin)
