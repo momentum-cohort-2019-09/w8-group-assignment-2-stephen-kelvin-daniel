04bb07e7b04007e2a6876492e4a54e20a53f5284
danielwalker <dawalker2017@outlook.com>
2019-10-29 01:41:15 -0400

    Added admin "panel tabs" to each class for more detailed interface |01:33am|10.29.19|

--- Flash/models.py
+++ Flash/models.py
@@ -7,53 +7,67 @@ class User(AbstractUser):
 
 
 class Deck(models.Model):
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE,
-                             related_name="decks"
-                             )
-
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="decks",
+    )
     title = models.CharField(
         verbose_name="Deck Title",
         max_length=50,
         blank=False,
     )
-
     description = models.TextField(
         verbose_name="Deck Description",
         max_length=255,
         blank=True,
         null=True,
     )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+
+    def __str__(self):
+        return self.description
+
+    def number_of_cards(self):
+        return self.cards.count()
 
 
 class Card(models.Model):
-    deck = models.ForeignKey(Deck,
-                             on_delete=models.CASCADE,
-                             related_name="cards"
-                             )
-
+    deck = models.ForeignKey(
+        to=Deck,
+        on_delete=models.CASCADE,
+        related_name="cards"
+    )
     question = models.TextField(
         verbose_name="Question:",
         max_length=255,
         blank=True,
         null=True,
     )
-
     answer = models.TextField(
         verbose_name="Answer:",
         max_length=255,
         blank=True,
         null=True,
     )
-
     total_guesses = models.IntegerField(
         default=0,
     )
-
     total_correct_guesses = models.IntegerField(
         default=0,
     )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.question
+
+    def __str__(self):
+        return self.answer
+
