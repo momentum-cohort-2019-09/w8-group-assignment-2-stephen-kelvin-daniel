07b1d3823a408eaa0f4dd729d00b4cdac3efe00c
danielwalker <dawalker2017@outlook.com>
2019-10-29 22:53:12 -0400

    Created temporary urls for testing a live interface and design

--- Flash/views.py
+++ Flash/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, 'Flash/dashboard.html')
+
+
+def testing(request):
+    return render(request, 'Flash/testing.html')
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Flash import views
 
 urlpatterns = [
+    # path('', views.dashboard),
+    path('', views.testing),
     path('admin/', admin.site.urls),
 ]
