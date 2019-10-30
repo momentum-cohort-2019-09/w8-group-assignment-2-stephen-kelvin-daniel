5ddd730638789c9f2bdfb306de7e951363764128
danielwalker <dawalker2017@outlook.com>
2019-10-30 19:52:14 -0400

    Created a view, template, and stylesheet for Dashboard.html

--- Flash/views.py
+++ Flash/views.py
@@ -1,17 +1,16 @@
-# from django.shortcuts import render
-
-
-
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from Flash.models import User, Deck, Card
 
+def dashboard(request):
+    all_decks = Deck.objects.all()
+    return render(request, "Flash/dashboard.html", {
+        "decks": all_decks,
+    })
 
-# Create your views here.
 def testing(request):
     return render(request, 'Flash/testing.html')
 
-def dashboard(request):
-    return render(request, 'Flash/dashboard.html')
 
 # @ login_required
 # def index_views(request):
