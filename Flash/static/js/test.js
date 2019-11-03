32510e6097e669557094bd46870a3eefb4cdd35e
danielwalker <dawalker2017@outlook.com>
2019-11-03 18:42:40 -0500

    WIP

--- Flash/static/js/test.js
+++ Flash/static/js/test.js
@@ -1,6 +1,20 @@
+//========================================================================| card flip in <test-deck.html> |============\\
 console.log('<test_deck> js loaded')
 
 function flip() {
     card.classList.toggle('flipped');
 };
 document.getElementById('container').addEventListener('click', flip)
+
+//========================================================================| guesses in <test-deck.html> |============\\
+console.log('starting feedback...')
+
+function correct() {
+    console.log('correct clicked!')
+};
+document.getElementById('button_right').addEventListener('click', correct)
+
+function incorrect() {
+    console.log('incorrect clicked!')
+};
+document.getElementById('button_wrong').addEventListener('click', incorrect)
